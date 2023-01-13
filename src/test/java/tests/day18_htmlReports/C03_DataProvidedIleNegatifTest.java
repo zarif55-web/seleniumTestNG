4111ff1b8a0093ed52ceb134eec4b10cf5f9742d
unknown <tercanliogluz@gmail.com>
2023-01-13 20:32:14 +0300

    19.gun sonu

---
+++ src/test/java/tests/day18_htmlReports/C03_DataProvidedIleNegatifTest.java
@@ -0,0 +1,46 @@
+package tests.day18_htmlReports;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C03_DataProvidedIleNegatifTest {
+    // Qualitydemy anasayfaya gidin
+    // verilen yanlis kullanici adi ve password kombinasyonlari icin
+    // giris yapilamadigini test edin
+    // username     password
+    // A11          A12345
+    // B12          B12345
+    // C13          C12345
+    // D14          D12345
+    // E15          E12345
+
+    @DataProvider
+    public static Object[][] kullaniciBilgileri() {
+
+        Object[][] kullaniciBilgileriArr=
+                {{"A11","A12345"},{"B12","B12345"},{"C13","C12345"},{"D14","D12345"},{"E15","E12345"}};
+        return kullaniciBilgileriArr;
+    }
+
+    @Test(dataProvider = "kullaniciBilgileri")
+    public void negativeLoginTest(String username, String password) {
+
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+
+        QualitydemyPage qualitydemyPage = new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
+
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys(username);
+        qualitydemyPage.passwordKutusu.sendKeys(password);
+        qualitydemyPage.cookieButton.click();
+        ReusableMethods.bekle(2);
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+    }
+}
